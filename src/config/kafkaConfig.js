2e8187c778dbd22ba0da84a7b6e0ac2856018a98
Muskan Agarwal <muskan.agarwal@inito.com>
2025-02-03 15:22:21 +0530

    integrating logs to elastic

--- src/config/kafkaConfig.js
+++ src/config/kafkaConfig.js
@@ -1,16 +1,17 @@
 const { Kafka, logLevel } = require('kafkajs');
-const fs = require('fs');
+// const fs = require('fs');
 
 const kafka = new Kafka({
     clientId: process.env.KAFKA_CLIENT_ID,
-    brokers: [process.env.BROKER_1],
-    ssl: {
-        rejectUnauthorized: false, // Set to false only for debugging
-        ca: [fs.readFileSync(process.env.CA_PEM)],
-        key: fs.readFileSync(process.env.CLIENT_KEY),
-        cert: fs.readFileSync(process.env.CLIENT_CERT),
-        passphrase: process.env.PASSWORD,
-      },
+    brokers: [process.env.BROKER_1, process.env.BROKER_1],
+    ssl: undefined,
+    // ssl: {
+    //     rejectUnauthorized: false, // Set to false only for debugging
+    //     ca: [fs.readFileSync(process.env.CA_PEM)],
+    //     key: fs.readFileSync(process.env.CLIENT_KEY),
+    //     cert: fs.readFileSync(process.env.CLIENT_CERT),
+    //     passphrase: process.env.PASSWORD,
+    //   },
 });
 
 module.exports = { kafka, logLevel };
